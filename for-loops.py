4c2dc872fe045d49dc6412aee4ec6e0f4029edb3
Andre Gonzaga <andre.gonzaga1@ibm.com>
2023-05-19 10:19:59 +0100

    adding if and for loops

---
+++ for-loops.py
@@ -0,0 +1,67 @@
+# For loops
+
+
+mylist = [1,2,3,4,5,6,7,8,9,10]
+for num in mylist:
+    print(num)
+
+
+
+
+for num in mylist:
+    # Check for even
+    if num % 2 == 0:
+        print(num)
+    else:
+        print(f'Odd number: {num}')
+
+
+list_sum = 0
+for num in mylist:
+    list_sum = list_sum + num
+
+print(list_sum)
+
+
+mystring = 'Hello World'
+for letter in mystring:
+    print(letter)
+
+
+tup = (1,2,3)
+for item in tup:
+    print(item)
+
+
+
+mylist = [(1,2),(3,4),(5,6),(7,8)]
+for tup in mylist:
+    print(tup)
+
+
+for (a,b) in mylist:
+    print(a)
+    print(b)
+
+for (a,b) in mylist:
+    print(a)
+
+
+d = {'k1':1,'k2':2,'k3':3}
+for key,value in d.items():
+#    print(key)
+#    print(value)
+#    print('==============')
+    print(f'the key is {key} and the value is {value}')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ if-elif-else.py
@@ -0,0 +1,58 @@
+# If else elif
+# if some_condition:
+#    execute some code
+# else:
+#    do something else
+
+
+
+
+if True:
+    print('Its TRUE!!!')
+
+
+
+hungry = True
+
+if hungry:
+    print('Feed me')
+
+
+hungry = False
+if hungry:
+    print('Feed me!')
+else:
+    print('I am not hungry')
+
+
+
+
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print('Cars are cool')
+elif loc == 'Bank':
+    print('Money is cool')
+elif loc == 'Store':
+    print('Welcome to store')
+else:
+    print('I do not know much')
+
+
+
+
+name = 'Sammy'
+
+if name == 'Frankie':
+    print('Hello Frankie')
+elif name == 'Sammy':
+    print('Hello Sammy')
+else:
+    print('What is your name')
+
+
+
+
+
+
+
